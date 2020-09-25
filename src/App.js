48b9f5044ec526c5902908597b63b4671719ccd6
luu.hoait <luu.hoait@gmail.com>
2020-09-25 16:38:08 +0700

    backup

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import React, { Component } from 'react';
+import { Route } from 'react-router-dom';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
 
-class App extends Component {
-  render() {
-    return <div>
-        <HomePage></HomePage>
-    </div>;
-  }
+const HatsPage = () =>(
+    <div>
+        <h1>HATS PAGE</h1>
+    </div>
+);
+
+function App(){
+ return (
+  <div>
+      <Route path='/' component={HomePage} />
+      <Route path='/hats' component={HatsPage} />
+  </div>
+ )
 }
 
 export default App;
